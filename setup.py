f480f9f611d54585d0703f4f4467569ac4946d43
Agilarasu Saravanan <agilarasusaravanan@gmail.com>
2024-09-07 16:33:03 +0530

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="clashinfo",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python wrapper for the Clash of Clans and Clash Royale API",
     author="Agilarasu Saravanan",
     author_email="s.agilarasu@outlook.com",
